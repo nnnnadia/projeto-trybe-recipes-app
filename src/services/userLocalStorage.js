2337b02166a228341630fa759a3238ab6e0e5bc0
Tayna-Macedo <tayna_sm1996@hotmail.com>
2022-07-17 11:26:41 -0300

    Correção no localStorage

--- src/services/userLocalStorage.js
+++ src/services/userLocalStorage.js
@@ -2,7 +2,13 @@
 //   localStorage.setItem('user', JSON.stringify({ email: '' }));
 // }
 
-export const readStorageUserData = () => JSON.parse(localStorage.getItem('user'));
+export const readStorageUserData = () => {
+  if (!localStorage.getItem('user')) {
+    localStorage.setItem('user', JSON.stringify({ email: '' }));
+  }
+
+  return JSON.parse(localStorage.getItem('user'));
+};
 
 export const saveStorageUserData = (email) => {
   localStorage.setItem('user', JSON.stringify({ email }));

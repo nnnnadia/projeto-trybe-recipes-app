77863cd9853e80fd4f9966e78a9d2ebdd70eb425
Tayna-Macedo <tayna_sm1996@hotmail.com>
2022-07-17 20:27:49 -0300

    Requisito 25

---
+++ src/components/IngredientsList.js
@@ -0,0 +1,55 @@
+import React, { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
+
+function IngredientsList({ details }) {
+  const [ingredients, setIngredients] = useState([]);
+  const [measures, setMeasures] = useState([]);
+
+  console.log(details);
+
+  useEffect(() => {
+    const onlyIngredients = { ...details };
+    const onlyMeasures = { ...details };
+
+    Object.keys(onlyIngredients).forEach((key) => {
+      if (!key.includes('strIngredient')) {
+        delete onlyIngredients[key];
+      }
+    });
+
+    Object.keys(onlyMeasures).forEach((key) => {
+      if (!key.includes('strMeasure')) {
+        delete onlyMeasures[key];
+      }
+    });
+
+    setIngredients(Object.values(onlyIngredients));
+    setMeasures(Object.values(onlyMeasures));
+  }, [details]);
+
+  return (
+    <>
+      <h5>Ingredientes</h5>
+      <ul>
+        {ingredients
+          .filter((ingredient) => ingredient && ingredient.length > 0)
+          .map((ingredient, index) => (
+            <li
+              key={ ingredient }
+              data-testid={ `${index}-ingredient-name-and-measure` }
+            >
+              {measures[index] && measures[index].length > 0
+                ? `${measures[index]} - ${ingredient}`
+                : `${ingredient}`}
+            </li>
+          ))}
+      </ul>
+    </>
+  );
+}
+
+IngredientsList.propTypes = {
+  details: PropTypes.shape().isRequired,
+};
+
+export default IngredientsList;
--- src/components/RecipeDetails.js
+++ src/components/RecipeDetails.js
@@ -1,16 +1,17 @@
 import React, { useEffect, useState } from 'react';
 import { useHistory, useParams } from 'react-router-dom';
+import IngredientsList from './IngredientsList';
 import { fetchDetailsDrink, fetchDetailsFood } from '../data';
 
 function RecipeDetails() {
   const [details, setDetails] = useState({});
 
   const history = useHistory();
+  const isFood = history.location.pathname.includes('/food');
+
   const { id } = useParams();
 
   useEffect(() => {
-    const isFood = history.location.pathname.includes('/food');
-
     const getDetails = async () => {
       if (isFood) {
         const foodDetails = await fetchDetailsFood(id);
@@ -24,7 +25,40 @@ function RecipeDetails() {
     getDetails();
   }, []);
 
-  return <div>RecipeDetails</div>;
+  const editUrlVideo = () => {
+    if (details.strYoutube) {
+      const initialUrl = details.strYoutube;
+      const edited = initialUrl.replace('watch?v=', 'embed/');
+
+      return edited;
+    }
+  };
+
+  return (
+    <div>
+      <img
+        data-testid="recipe-photo"
+        src={ details.strMealThumb || details.strDrinkThumb }
+        alt="Imagem da receita"
+      />
+      <h3 data-testid="recipe-title">{details.strMeal || details.strDrink}</h3>
+      <h4 data-testid="recipe-category">
+        {isFood ? details.strCategory : details.strAlcoholic}
+      </h4>
+      <IngredientsList details={ details } />
+      <p data-testid="instructions">{details.strInstructions}</p>
+      {isFood && (
+        <iframe
+          data-testid="video"
+          width="420"
+          height="315"
+          src={ editUrlVideo() }
+          title="Video no Youtube"
+        />
+      )}
+      <p data-testid="0-recomendation-card">Recomendadas</p>
+    </div>
+  );
 }
 
 export default RecipeDetails;

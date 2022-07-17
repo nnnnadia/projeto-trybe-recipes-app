98da57707040831e067f54e70b5f2cbbe4265d97
Tayna-Macedo <tayna_sm1996@hotmail.com>
2022-07-17 09:14:23 -0300

    Requisito 57

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
 function Profile() {
+  useEffect(() => {}, []);
+
   return (
     <div>
       <Header pageTitle="Profile" showSearchIcon={ false } />
+      <h4 data-testid="profile-email">tayna_sm1996@hotmail.com</h4>
+      <button type="button" data-testid="profile-done-btn">
+        Receitas feitas
+      </button>
+      <button type="button" data-testid="profile-favorite-btn">
+        Receitas favoritas
+      </button>
+      <button type="button" data-testid="profile-logout-btn">
+        Sair
+      </button>
       <Footer />
     </div>
   );

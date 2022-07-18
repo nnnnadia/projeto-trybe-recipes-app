88b7b45572e9b8d474f507f1fe97714457c551a6
Tayna-Macedo <tayna_sm1996@hotmail.com>
2022-07-18 10:29:26 -0300

    Requisito 27

--- src/components/Recommended.js
+++ src/components/Recommended.js
@@ -2,6 +2,7 @@ import React, { useContext } from 'react';
 import { useHistory } from 'react-router-dom';
 import RecipesContext from '../context/RecipesContext';
 import CardRecipe from './CardRecipe';
+import '../styles/Recommended.css';
 
 function Recommended() {
   const { allFoods, allDrinks } = useContext(RecipesContext);
@@ -14,31 +15,33 @@ function Recommended() {
   return (
     <>
       <h4>Recommended</h4>
-      {isFood
-        ? allDrinks
-          .slice(0, MAX_ITEMS)
-          .map((drink, index) => (
-            <CardRecipe
-              key={ drink.idDrink }
-              image={ drink.strDrinkThumb }
-              title={ drink.strDrink }
-              index={ index }
-              id={ drink.idDrink }
-              recomendation
-            />
-          ))
-        : allFoods
-          .slice(0, MAX_ITEMS)
-          .map((food, index) => (
-            <CardRecipe
-              key={ food.idMeal }
-              image={ food.strMealThumb }
-              title={ food.strMeal }
-              index={ index }
-              id={ food.idMeal }
-              recomendation
-            />
-          ))}
+      <div className="scrollmenu">
+        {isFood
+          ? allDrinks
+            .slice(0, MAX_ITEMS)
+            .map((drink, index) => (
+              <CardRecipe
+                key={ drink.idDrink }
+                image={ drink.strDrinkThumb }
+                title={ drink.strDrink }
+                index={ index }
+                id={ drink.idDrink }
+                recomendation
+              />
+            ))
+          : allFoods
+            .slice(0, MAX_ITEMS)
+            .map((food, index) => (
+              <CardRecipe
+                key={ food.idMeal }
+                image={ food.strMealThumb }
+                title={ food.strMeal }
+                index={ index }
+                id={ food.idMeal }
+                recomendation
+              />
+            ))}
+      </div>
     </>
   );
 }
